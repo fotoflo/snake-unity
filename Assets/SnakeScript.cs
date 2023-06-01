4f9d110f1fbc586dd89234f4cbc2af5bdd52688b
Alex Miller <fotoflo@gmail.com>
2023-06-02 01:02:25 +0800

    snake is now working

--- Assets/SnakeScript.cs
+++ Assets/SnakeScript.cs
@@ -18,43 +18,44 @@ public class SnakeScript : MonoBehaviour
 
     public void Start()
     {
-         Time.fixedDeltaTime = 0.02f;
+         Time.fixedDeltaTime = 0.1f;
          unroundedPosition = transform.position;
          _segments = new List<Transform>();
+         _segments.Add(this.transform);
     }
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) {
             _direction = Vector2.left;
-            MoveOne();
         } else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
         {
             _direction = Vector2.right;
-            MoveOne();
         } else if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
         {
             _direction = Vector2.up;
-            MoveOne();
         } else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
         {
             _direction = Vector2.down;
-            MoveOne();
         }
 
     }
 
     private void FixedUpdate()
     {
+        for( int i = _segments.Count - 1; i > 0;  i--){
+            _segments[i].position = _segments[i - 1].position;
+        }
+
         MoveOne();
     }
 
     private void Grow()
     {
         Transform segment = Instantiate(this.segmentPrefab);
-
-        _segments.Add(segment);
+        
         segment.position = _segments[_segments.Count - 1].position;
+        _segments.Add(segment);
     }
 
 
@@ -80,8 +81,11 @@ public class SnakeScript : MonoBehaviour
 
     private void MoveOne()
     {
-        unroundedPosition += new Vector3(_direction.x * speed, _direction.y * speed, 0);
-        transform.position = new Vector3(Mathf.Round(unroundedPosition.x), Mathf.Round(unroundedPosition.y), 0);
+        this.transform.position = new Vector3(
+            Mathf.Round(this.transform.position.x) + _direction.x,
+            Mathf.Round(this.transform.position.y) + _direction.y,
+            0.0f
+        );
     }
 
 }
