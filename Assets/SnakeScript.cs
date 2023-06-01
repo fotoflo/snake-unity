bec66114f2ce94d18eb71b3444cfb3965d8b5403
Alex Miller <fotoflo@gmail.com>
2023-06-02 02:32:04 +0800

    seems like we're almost done

--- Assets/SnakeScript.cs
+++ Assets/SnakeScript.cs
@@ -6,8 +6,9 @@ public class SnakeScript : MonoBehaviour
 {
 
     private Vector2 _direction = Vector2.up;
-    private Vector3 unroundedPosition;
+    private Vector2 input;
 
+    private Vector3 unroundedPosition;
     private Vector3 originalPosition;
 
     private List<Transform> _segments;
@@ -16,33 +17,44 @@ public class SnakeScript : MonoBehaviour
 
     public float speed;
 
+    public int intialSize = 4;
+
     public void Start()
     {
          Time.fixedDeltaTime = 0.1f;
          unroundedPosition = transform.position;
          _segments = new List<Transform>();
-         _segments.Add(this.transform);
+         ResetState();
     }
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) {
-            _direction = Vector2.left;
-        } else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            _direction = Vector2.right;
-        } else if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            _direction = Vector2.up;
-        } else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            _direction = Vector2.down;
-        }
 
+    private void Update()
+    {
+        // Only allow turning up or down while moving in the x-axis
+        if (_direction.x != 0f)
+        {
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) {
+                input = Vector2.up;
+            } else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) {
+                input = Vector2.down;
+            }
+        }
+        // Only allow turning left or right while moving in the y-axis
+        else if (_direction.y != 0f)
+        {
+            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) {
+                input = Vector2.right;
+            } else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) {
+                input = Vector2.left;
+            }
+        }
     }
 
     private void FixedUpdate()
     {
+        if (input != Vector2.zero) {
+            _direction = input;
+        }
+    
         for( int i = _segments.Count - 1; i > 0;  i--){
             _segments[i].position = _segments[i - 1].position;
         }
@@ -67,9 +79,15 @@ public class SnakeScript : MonoBehaviour
         {
             Destroy(_segments[i].gameObject);
         }
-
         _segments.Clear();
+
         _segments.Add(this.transform);
+
+        for ( int i = 1; i < intialSize; i++)
+        {
+            _segments.Add(Instantiate(this.segmentPrefab));
+        }
+
         this.transform.position = Vector3.zero;
     }
 
